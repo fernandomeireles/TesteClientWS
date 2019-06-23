1f989f280a0bf7ed7fb4a0816a11106ad4d31b3e
fernandomeireles <fernando.meireles.filho@gmail.com>
2019-06-23 18:50:32 -0300

    Consumindo apimock sem lista e demonstrando dados no terminal

--- ClientWS/src/main/java/br/com/TesteWS/ClientWS/App.java
+++ ClientWS/src/main/java/br/com/TesteWS/ClientWS/App.java
@@ -1,13 +1,59 @@
 package br.com.TesteWS.ClientWS;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+import javax.xml.ws.Service;
+
+import br.com.TesteWS.Entity.MotorRegra;
+import br.com.TesteWS.Service.MotorRegraService;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        
+    	 Scanner ler = new Scanner(System.in);
+		 
+		 String escolha;
+		 		 
+				// DECLARANDO O OBJETO DA NOSSA CLASSE QUE ACESSA O SERVIÇO REST
+			 MotorRegraService client = new  MotorRegraService();
+		 
+				// CAPTURA O CÓDIGO A SER CONSULTADO
+			 System.out.println( "Digite um numero de ficha" );
+		    
+		     escolha = ler.next();
+		 
+				// CONSULTA UMA PESSOA PELO CÓDIGO
+				MotorRegra motorRegra = client.ConsultarGlosa(Integer.parseInt(escolha));
+		 
+				if (motorRegra == null) {
+		 
+					JOptionPane.showMessageDialog(null, "Sem Ficha !");
+		 
+					// CHAMANDO A FUNÇÃO PRINCIPAL DO NOSSO SISTEMA
+					main(null);
+				} else {
+		 
+					String resultado = null;
+		 
+					// MONTA O RESULTADO PARA MOSTRARMOS NA MENSAGEM DE SAIDA
+					resultado = "Ficha: " + String.valueOf(motorRegra.getCdFicha()) + "\n";
+					resultado += "NroSeqInclusao:   " + String.valueOf(motorRegra.getNroSeqInclusao()) + "\n";
+					resultado += "cdLozalicao:   "  + motorRegra.getCdLocalizao() + "\n";
+					resultado += "Evento:   "  + String.valueOf(motorRegra.getCdEvento()) + "\n";
+					resultado += "Glosa:   " + String.valueOf(motorRegra.getCdGlosa()) + "\n";
+		 
+					// MENSAGEM COM O RESULTADO
+					JOptionPane.showMessageDialog(null, resultado);
+		 
+					// CHAMANDO A FUNÇÃO PRINCIPAL DO NOSSO SISTEMA
+					main(null);
+				}
+		 
+			}
+
+	
 }
--- ClientWS/src/main/java/br/com/TesteWS/Service/MotorRegraService.java
+++ ClientWS/src/main/java/br/com/TesteWS/Service/MotorRegraService.java
@@ -1,5 +1,45 @@
 package br.com.TesteWS.Service;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.xml.ws.Response;
+
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.glassfish.jersey.client.ClientConfig;
+
+import br.com.TesteWS.Entity.MotorRegra;
+
 public class MotorRegraService {
 
+	
+	private Client client;
+	 
+	
+	private WebTarget webTarget;
+ 
+
+	private final String URL_SERVICE = "http://5d09201b034e500014010ef2.mockapi.io/motor-regras-processamento/resources/execute/";
+ 
+	/**CONSTRUTOR DA NOSSA CLASSE*/
+	public MotorRegraService(){
+ 
+		this.client = ClientBuilder.newClient(new ClientConfig(JacksonJsonProvider.class));
+	}
+	
+	public MotorRegra ConsultarGlosa(int cdFicha){
+		 
+		this.webTarget = this.client.target(URL_SERVICE).path("cdFicha/").path(String.valueOf(cdFicha));
+ 
+		Invocation.Builder invocationBuilder =  this.webTarget.request(MediaType.APPLICATION_JSON);
+ 
+		javax.ws.rs.core.Response response = invocationBuilder.get();
+ 
+		return response.readEntity(MotorRegra.class);
+ 
+	}
+	
+	
 }
