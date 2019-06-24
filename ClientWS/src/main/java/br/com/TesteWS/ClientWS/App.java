35c40bd27004766a837a89c7d08f420a28838a35
fernandomeireles <fernando.meireles.filho@gmail.com>
2019-06-23 22:47:45 -0300

    Aplicação gera lista de glosas em formato de string junto a ficha origem, porem sem nenhum tratamento

--- ClientWS/src/main/java/br/com/TesteWS/ClientWS/App.java
+++ ClientWS/src/main/java/br/com/TesteWS/ClientWS/App.java
@@ -17,57 +17,69 @@ public class App
     public static void main( String[] args )
     {
     	int controlador =0;
-    	
+
     	while(controlador==0) {
-    		
-    		
-        
+
+
+
     	 Scanner ler = new Scanner(System.in);
-		 
+
 		 String escolha;
-		 		 
-				
+
+				// DECLARANDO O OBJETO DA NOSSA CLASSE QUE ACESSA O SERVIÇO REST
+
 			 MotorRegraService client = new  MotorRegraService();
-		 
-				
+
+				// CAPTURA O CÓDIGO A SER CONSULTADO
+			 System.out.println( "Digite um numero de ficha" );
+
 			 System.out.println( "\n Digite um numero de ficha para ser procurado na API e gravada no banco:  \n" );
-		    
+
 		     escolha = ler.next();
-		 
-				
-		     MotorGlosas motorglosas = client.GerarGlosas(Integer.parseInt(escolha));
-		     
-		     StringBuilder stringBuiderDetalhesMotor = new StringBuilder();
-					
-		     for (MotorRegra motorregra : motorglosas) {
-		    	 
-		    	stringBuiderDetalhesMotor.append("Ficha: ");
-		    	stringBuiderDetalhesMotor.append(String.valueOf(((MotorRegra) motorglosas).getCdFicha()));
-		    	stringBuiderDetalhesMotor.append(" NroSeqInclusao: ");
-				stringBuiderDetalhesMotor.append(String.valueOf(((MotorRegra) motorglosas).getNroSeqInclusao()));
-				stringBuiderDetalhesMotor.append(" Localizacao: ");
-				stringBuiderDetalhesMotor.append(((MotorRegra) motorglosas).getCdLocalizao());
-				stringBuiderDetalhesMotor.append(" Evento: ");
-		    	stringBuiderDetalhesMotor.append(String.valueOf(((MotorRegra) motorglosas).getCdEvento()));
-		    	stringBuiderDetalhesMotor.append(" Glosa: ");
-				stringBuiderDetalhesMotor.append(String.valueOf(((MotorGlosas) motorglosas).getListcdGlosa()));
-				stringBuiderDetalhesMotor.append("\n\n");
-		 
-				}		
-					
-		     System.out.println(stringBuiderDetalhesMotor.toString());
-					
-					System.out.println("\n Deseja executar novamente ? \n");
-					controlador = ler.nextInt();
+
+				// CONSULTA UMA PESSOA PELO CÓDIGO
+
+				MotorRegra motorRegra = client.ConsultarGlosa(Integer.parseInt(escolha));
+
+				if (motorRegra == null) {
+
+					JOptionPane.showMessageDialog(null, "Sem Ficha !");
+					System.out.println("Sem Ficha !");
+
+					// CHAMANDO A FUNÇÃO PRINCIPAL DO NOSSO SISTEMA
+
+					main(null);
+				} else {
+
+					String resultado = null;
+
+					// MONTA O RESULTADO PARA MOSTRARMOS NA MENSAGEM DE SAIDA
+
+					resultado = "Ficha: " + String.valueOf(motorRegra.getCdFicha()) + "\n";
+					resultado += "NroSeqInclusao:   " + String.valueOf(motorRegra.getNroSeqInclusao()) + "\n";
+					resultado += "cdLozalicao:   "  + motorRegra.getCdLocalizao() + "\n";
+					resultado += "Evento:   "  + String.valueOf(motorRegra.getCdEvento()) + "\n";
+					resultado += "Glosa:   " + String.valueOf(motorRegra.getCdGlosa()) + "\n";
+
+					System.out.println(resultado);
+
+					MotorRegraDAO motorregraDAO = new MotorRegraDAO();
+					motorregraDAO.adiciona (motorRegra);
 							
 					
-					
-					
+					main(null);
+					System.out.println("\n Deseja executar novamente ? \n");
+					controlador = ler.nextInt();
+
+
+
+
 				}
-		 
+
 			}
-    	
-    	
+
+    	System.exit(0);
+    }
     }
 
 	
